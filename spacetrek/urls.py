8e67cfc2d0193dfda1b0dff54e15f5e4226da1f7
Dhruv Kalra <kalradhruv2003@gmail.com>
2023-10-08 03:11:38 +0530

    Ticket booking system integrated

--- spacetrek/urls.py
+++ spacetrek/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for spacetrek project.
+"""spacetrek URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include("client.urls")),
-]
+    path('',include("flight.urls")),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
